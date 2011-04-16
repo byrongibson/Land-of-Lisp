504e12dac250f8bdbae3b1f8d69902837113e92b
Byron Gibson <fbg111@gmail.com>
2011-04-16 14:01:22 -0700

    update

--- mud.lisp
+++ mud.lisp
@@ -1,14 +1,24 @@
+
+; global vars
 (defparameter *nodes* '((living-room (you are in the living room. a wizard is snoring loudly on the couch.))
 						(garden (you are in a beautiful garden.
 									 there is a well in front of you.))
 						(attic (you are in the attic.  there is a giant welding torch in the corner.))))
 
-(defun describe-location (location nodes)
-  (cadr (assoc location nodes)))
-
 (defparameter *edges* '((living-room (garden west door) (attic upstairs ladder))
 						(attic (living-room downstairs ladder))
 						(garden (living-room east door))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+								   (bucket living-room)
+								   (chain garden)
+								   (frog garden)))
+
+; function defs 
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
